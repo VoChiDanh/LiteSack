1040e439f7d530711801b5937bc99124c4fd19ee
VoChiDanh <vochidanh.hs.c@gmail.com>
2023-02-05 04:13:03 +0700

    replace name, 64 -> 256

--- src/main/java/net/danh/litesack/API/Data/Player/PlayerData.java
+++ src/main/java/net/danh/litesack/API/Data/Player/PlayerData.java
@@ -172,9 +172,9 @@ public class PlayerData {
                                 if (decreaseSackData(p, sackID, item, String.valueOf(number))) {
                                     p.getInventory().addItem(new ItemStack(material, number));
                                     removed.set(true);
-                                    Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.WITHDRAW_SUCCESS")).replace("<name>", item_data).replace("<amount>", String.valueOf(number)));
+                                    Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.WITHDRAW_SUCCESS")).replace("<name>", new ItemStack(material).getItemMeta().getDisplayName()).replace("<amount>", String.valueOf(number)));
                                 } else {
-                                    Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.NOT_ENOUGH")).replace("<name>", item_data).replace("<withdraw>", String.valueOf(number)).replace("<amount>", String.valueOf(SackData.pSackData.get(p.getName() + "_" + item))));
+                                    Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.NOT_ENOUGH")).replace("<name>", new ItemStack(material).getItemMeta().getDisplayName()).replace("<withdraw>", String.valueOf(number)).replace("<amount>", String.valueOf(SackData.pSackData.get(p.getName() + "_" + item))));
                                 }
                             } else {
                                 Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.MATERIAL_IS_NULL")).replace("<name>", item_data));
@@ -193,9 +193,9 @@ public class PlayerData {
                                     if (decreaseSackData(p, sackID, item, String.valueOf(number))) {
                                         p.getInventory().addItem(mmo);
                                         removed.set(true);
-                                        Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.WITHDRAW_SUCCESS")).replace("<name>", item_data).replace("<amount>", String.valueOf(number)));
+                                        Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.WITHDRAW_SUCCESS")).replace("<name>", mmo.getItemMeta().getDisplayName()).replace("<amount>", String.valueOf(number)));
                                     } else {
-                                        Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.NOT_ENOUGH")).replace("<name>", item_data).replace("<withdraw>", String.valueOf(number)).replace("<amount>", String.valueOf(SackData.pSackData.get(p.getName() + "_" + item))));
+                                        Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.NOT_ENOUGH")).replace("<name>", mmo.getItemMeta().getDisplayName()).replace("<withdraw>", String.valueOf(number)).replace("<amount>", String.valueOf(SackData.pSackData.get(p.getName() + "_" + item))));
                                     }
                                 } else {
                                     Chat.sendPlayerMessage(p, Objects.requireNonNull(File.getMessage().getString("COMMAND.WITHDRAW.MATERIAL_IS_NULL")).replace("<name>", item_data));
